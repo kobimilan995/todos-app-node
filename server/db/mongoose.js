9b525d4e6095fecae6f15faedae48c0923c71619
Milan Urosevic <kobimilan995@yahoo.com>
2017-11-14 22:18:03 +0100

    setting up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+mongoose.connect('mongodb://kobimilan995:yrosevic111@ds259865.mlab.com:59865/kobicarevabaza', { useMongoClient: true });
 
 module.exports = {
 	mongoose
