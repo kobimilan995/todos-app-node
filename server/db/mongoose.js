9a482d78d4442c808e69fba49644c5fc6e5674e5
Milan Urosevic <kobimilan995@yahoo.com>
2017-11-15 23:14:08 +0100

    more config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+if(process.env.NODE_ENV === 'production'){
+	mongoose.connect('mongodb://kobimilan995:yrosevic111@ds259865.mlab.com:59865/kobicarevabaza', { useMongoClient: true });
+} else {
 mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+}
 // if(process.env.PORT){
 // 	mongoose.connect('mongodb://kobimilan995:yrosevic111@ds259865.mlab.com:59865/kobicarevabaza', { useMongoClient: true });
 // } else {
