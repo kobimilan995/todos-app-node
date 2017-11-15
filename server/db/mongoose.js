347e11c14f05b55280de5757955074c48c967e95
Milan Urosevic <kobimilan995@yahoo.com>
2017-11-15 23:11:57 +0100

    mongodb uri config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-if(process.env.PORT){
-	mongoose.connect('mongodb://kobimilan995:yrosevic111@ds259865.mlab.com:59865/kobicarevabaza', { useMongoClient: true });
-} else {
-	mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
-}
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+// if(process.env.PORT){
+// 	mongoose.connect('mongodb://kobimilan995:yrosevic111@ds259865.mlab.com:59865/kobicarevabaza', { useMongoClient: true });
+// } else {
+	
+// 	console.log('duvaj ga', process.env.MONGODB_URI);
+// }
 
 
 module.exports = {
